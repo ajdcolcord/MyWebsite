680f63b581a03362a343c983befd8a29c552740c
Austin Colcord <ajdcolcord@gmail.com>
2015-10-09 15:03:04 -0400

    Updating git repository with many updates and files to the website

---
+++ main.js
@@ -0,0 +1,28 @@
+function clickPicture() {
+	document.getElementById("selfPicture").onclick = function() {
+		var thisId = "selfPicture";
+		var currentWidth = document.getElementById(thisId).style.width;
+		if (currentWidth == "100px") {
+			setPictureSize("200px", thisId);
+		}
+		else {
+			setPictureSize("100px", thisId);
+		}
+	}
+}
+
+
+function clickPhoto() {
+	document.getElementById("photo1").onclick = function() {
+		if (confirm('Would you like to download this picture?')) {
+			alert("SAVE");
+		}
+		else {
+			alert("DONT SAVE");
+		}
+	}
+}
+
+function setPictureSize(size, id) {
+	document.getElementById(id).style.width=size;
+}
